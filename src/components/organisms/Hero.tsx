bd8fb8490466eb17bb65b98d1b7bd5bdf8f6b9f1
David Space <genarodavid@gmail.com>
2024-08-20 13:39:09 -0400

    added new nnnoise

--- src/components/organisms/Hero.tsx
+++ src/components/organisms/Hero.tsx
@@ -4,7 +4,7 @@ import RandomQuote from "../atoms/RandomQuote";
 
 export default function Hero() {
   const textClassNames: string =
-    " text-[3.8rem] md:text-[6rem] leading-8 md:leading-[3rem]  text-stroke-[5px] text-stroke text-stroke-white text-stroke-outside  ";
+    " text-[3.8rem] md:text-[6rem] leading-8 md:leading-[3rem]  text-stroke-[5px] text-stroke text-stroke-outside   ";
   return (
     <header
       className="
@@ -16,7 +16,7 @@ export default function Hero() {
     relative
     "
     >
-      <div className="w-full opacity-60 bg-pattern-noisy"></div>
+      <div className="w-full opacity-30 bg-pattern-noisy"></div>
       <div className="grid relative grid-rows-2 w-full max-w-56 md:max-w-md">
         <h1 className={textClassNames + "text-left"}>David</h1>
         <h1 className={textClassNames + "text-right "}>Space</h1>
