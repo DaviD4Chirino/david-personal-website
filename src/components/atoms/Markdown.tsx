e664da03b5733f1f6d189925f79d7092736607a6
David Space <genarodavid@gmail.com>
2024-08-21 01:14:34 -0400

    installed hash-links

--- src/components/atoms/Markdown.tsx
+++ src/components/atoms/Markdown.tsx
@@ -1,18 +1,39 @@
-import Parser, { Options } from "react-markdown";
+import Parser, { Components, Options } from "react-markdown";
 import remarkGfm from "remark-gfm";
 import FigureImg from "./FigureImg";
+import { isValidElement } from "react";
+
+const components: Partial<Components> = {
+  p(props) {
+    const { children } = props;
+
+    if (isValidElement(children)) {
+      return <>{children}</>;
+    }
+
+    return <p>{children}</p>;
+  },
+  img(props) {
+    const { alt, src } = props;
+    return <FigureImg alt={alt || ""} src={src || ""} />;
+  },
+  a(props) {
+    const { children, node, ...rest } = props;
+    const anchorLinkRegex = /#/g;
+    const isAnchorLink = anchorLinkRegex.test(rest.href || "");
+
+    console.log(isAnchorLink);
+
+    return <a {...rest}>{children}</a>;
+  },
+};
 
 export default function Markdown(props: Options) {
   const { remarkPlugins, ...rest } = props;
   return (
     <Parser
       remarkPlugins={remarkPlugins ? remarkPlugins : [remarkGfm]}
-      components={{
-        img(props) {
-          const { alt, src } = props;
-          return <FigureImg alt={alt || ""} src={src || ""} />;
-        },
-      }}
+      components={components}
       {...rest}
     />
   );
