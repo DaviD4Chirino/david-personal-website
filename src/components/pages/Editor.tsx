fe12d2067eb3c335386f5692c949b019d60b6056
David Space <genarodavid@gmail.com>
2024-08-23 15:24:11 -0400

    added react hook

--- src/components/pages/Editor.tsx
+++ src/components/pages/Editor.tsx
@@ -2,8 +2,8 @@ import MarkdownEditor from "@uiw/react-markdown-editor";
 import InputLabel from "../molecules/InputLabel";
 import { useQuery } from "@tanstack/react-query";
 import { getAllArticles } from "../../database/getArticles";
-import EditArticleButton from "../molecules/editor/EditArticleButton";
 import BlogCard from "../molecules/BlogCard";
+import { SubmitHandler, useForm } from "react-hook-form";
 
 // TODO: Style this thing
 // TODO: Build the article object
@@ -17,72 +17,102 @@ export default function Editor() {
   });
 
   return (
-    <section className="container grid gap-16 my-10 ">
+    <section className="container  my-10 grid gap-y-28">
       <h1>Article Editor</h1>
-      <form action="" id="ArticleEditor">
-        <div className="grid grid-cols-1 md:grid-cols-6 gap-x-3 gap-y-8">
-          <InputLabel
-            name="article-name"
-            title="Name of the Article"
-            placeholder="in-kebab-case"
-            type="text"
-            className="md:col-span-2"
-          />
-          <InputLabel
-            name="article-title"
-            title="Title of the Article"
-            type="text"
-            className="md:col-span-2"
-          />
-          <InputLabel
-            name="article-description"
-            title="Description"
-            type="text"
-            className="md:col-span-2"
-          />
-          <InputLabel
-            name="article-category"
-            title="Category"
-            type="text"
-            className="md:col-span-2"
-          />
-          <InputLabel
-            name="article-tags"
-            title="Tags"
-            type="text"
-            className="md:col-span-2"
-          />
-          <InputLabel
-            name="article-date"
-            title="Date"
-            type="Date"
-            defaultValue={new Date().toLocaleDateString("en-CA")}
-            className="md:col-span-2"
-          />
-          <InputLabel
-            name="github-api-key"
-            title="Github Api Key"
-            type="password"
-            className="col-span-full"
-          />
-          <MarkdownEditor className="col-span-full " height="500px" />
-          <input
-            type="submit"
-            name="submit"
-            value="Submit"
-            className="
+      <Form />
+      {data && <ArticlesToEdit articles={data} />}
+    </section>
+  );
+}
+
+type Inputs = {
+  articleName: string;
+  exampleRequired: string;
+  articleTitle: string;
+  articleDescription: string;
+  articleCategory: string;
+  articleTags: string;
+  articleDate: string;
+  githubApiKey: string;
+};
+
+function Form() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      id="ArticleEditor"
+      className="grid grid-cols-1 md:grid-cols-6 gap-x-3 gap-y-8"
+    >
+      <InputLabel
+        title="Name of the Article"
+        placeholder="in-kebab-case"
+        type="text"
+        className="md:col-span-2"
+        {...register("articleName")}
+      />
+      <InputLabel
+        title="Title of the Article"
+        type="text"
+        className="md:col-span-2"
+        {...register("articleTitle")}
+      />
+      <InputLabel
+        title="Description"
+        type="text"
+        className="md:col-span-2"
+        {...register("articleDescription")}
+      />
+      <InputLabel
+        title="Category"
+        type="text"
+        className="md:col-span-2"
+        {...register("articleCategory")}
+      />
+      <InputLabel
+        title="Tags"
+        type="text"
+        className="md:col-span-2"
+        {...register("articleTags")}
+      />
+      <InputLabel
+        title="Date"
+        type="Date"
+        defaultValue={new Date().toLocaleDateString("en-CA")}
+        className="md:col-span-2"
+        {...register("articleDate")}
+      />
+      <InputLabel
+        title="Github Api Key"
+        type="password"
+        className="col-span-full"
+        {...register("githubApiKey")}
+      />
+      <MarkdownEditor className="col-span-full " height="500px" />
+      <input
+        type="submit"
+        name="submit"
+        value="Submit"
+        className="
             bg-tertiary-lightest hover:bg-tertiary-light 
             text-tertiary-darkest
             rounded-md px-3 py-5 
             col-span-full 
             cursor-pointer active:cursor-default 
             transition-colors
-            "
-          />
-        </div>
-      </form>
-      {data && <ArticlesToEdit articles={data} />}
-    </section>
+          "
+      />
+    </form>
   );
 }
 
@@ -93,15 +123,17 @@ function ArticlesToEdit(props: { articles: Articles }) {
       className="grid grid-cols-1 md:grid-cols-3 gap-6"
     >
       {props.articles &&
-        Object.values(props.articles).map((art) => (
-          <BlogCard
-            {...art}
-            to={{
-              search: `article=${art.name}`,
-            }}
-            key={art.id}
-          />
-        ))}
+        Object.values(props.articles)
+          .reverse()
+          .map((art) => (
+            <BlogCard
+              {...art}
+              to={{
+                search: `article=${art.name}`,
+              }}
+              key={art.id}
+            />
+          ))}
     </section>
   );
 }
