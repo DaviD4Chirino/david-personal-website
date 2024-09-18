7c3801f5d0072df0839183a1ae268abaa0c4e3c1
David Space <genarodavid@gmail.com>
2024-09-17 20:52:30 -0400

    preparing to migration

--- astro-site/astro.config.mjs
+++ astro-site/astro.config.mjs
@@ -11,9 +11,20 @@ import markdoc from '@astrojs/markdoc';
 
 import keystatic from '@keystatic/astro'
 
+import rehypeRewrite from 'rehype-rewrite';
+
+import remarkToc from "remark-toc"
+import remarkGfm from "remark-gfm"
+
+
 // https://astro.build/config
 export default defineConfig({
+  markdown:{
+    remarkPlugins :[remarkGfm,[remarkToc, { heading: "Contents"} ]],
+    
+  },
   integrations: [react(), tailwind(), markdoc(),keystatic()],
   output:"hybrid",
-  adapter: vercel()
+  adapter: vercel(),
+
 });
