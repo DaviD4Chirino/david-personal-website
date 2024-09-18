51432f6b46c8824a8edd7eb74e073ac1519b95eb
David Space <genarodavid@gmail.com>
2024-09-18 16:03:05 -0400

    added content in rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,6 +1,9 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 import routes from "../routes.json"
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+const parser = new MarkdownIt();
 
 export async function GET(context) {
     const posts = await getCollection('posts');
@@ -16,6 +19,9 @@ export async function GET(context) {
         category: post.data.category,
         tags: post.data.tags,
         link: `${routes.article}/${post.slug}/`,
+        content:  sanitizeHtml(parser.render(post.body), {
+          allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+        })
       })),
     customData: `<language>en-us</language>`,
   });
