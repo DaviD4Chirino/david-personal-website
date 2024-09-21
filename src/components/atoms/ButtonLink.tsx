c84de79b45f9ffb88188aef5b5f1653d69015a4a
David Space <genarodavid@gmail.com>
2024-09-21 16:32:41 -0400

    fixed BUttonLInk

--- src/components/atoms/ButtonLink.tsx
+++ src/components/atoms/ButtonLink.tsx
@@ -14,6 +14,28 @@ const coloredInteractiveClasses = (color: string, disabled?: boolean) =>
 		? `bg-${color} text-${color}-lightest`
 		: `bg-${color}-darkest text-${color}-lightest  hover:bg-${color}-dark active:bg-${color}-darkest`;
 
+const schemes = {
+	primary: {
+		classes:
+			"bg-primary-darkest text-primary-lightest  hover:bg-primary-dark active:bg-primary-darkest",
+		disabled: "bg-primary text-primary-lightest",
+	},
+	secondary: {
+		classes:
+			"bg-secondary-darkest text-secondary-lightest  hover:bg-secondary-dark active:bg-secondary-darkest",
+		disabled: "bg-secondary text-secondary-lightest",
+	},
+	tertiary: {
+		classes:
+			"bg-tertiary-darkest text-tertiary-lightest  hover:bg-tertiary-dark active:bg-tertiary-darkest",
+		disabled: "bg-tertiary text-tertiary-lightest",
+	},
+	grey: {
+		classes: "bg-grey-800 text-grey-200  hover:bg-grey-600 active:bg-grey-800",
+		disabled: "bg-grey text-grey-200",
+	},
+};
+
 const generalClasses: string = `
 		px-3 py-2
 		rounded
@@ -28,7 +50,9 @@ export default function ButtonLink<T extends { href: string }>(
 ) {
 	const { href, children, color = "primary", disabled, icon, ...rest } = props;
 
-	const theme: string = coloredInteractiveClasses(color, disabled);
+	const theme: string = disabled
+		? schemes[color].disabled
+		: schemes[color].classes;
 
 	if (href) {
 		if (disabled) {
